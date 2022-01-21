b05f5971c79dbfc691b716e05393dc0aa810f440
Rakesh Rohan <66308480+rakeshrohan-123@users.noreply.github.com>
2022-01-21 17:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 model1 = joblib.load("covid19_model.sav")
 model2 = pickle.load(open('heartmodel.pkl', 'rb'))
 model3=joblib.load("calories_model.sav")
+model4=joblib.load('cancer_model')
 
 
 dataset = pd.read_csv('diabetes.csv')
@@ -87,22 +88,62 @@ def home3():
 
 @app.route('/predict2',methods=['POST'])
 def prediction1():
-    gen1=    request.form['gen1']
+    gen1=   request.form['gen1']
     age1 = request.form['age1']
-    hei1=  request.form['hei1']
-    wei1=  request.form['wei1']
-    dur1=    request.form['dur1']
-    hra1=  request.form['hra1']
-    temp1= request.form['temp1']
-    h1=float(hei1)
-    w1=float(wei1)
-    d1=float(dur1)
-    h1=float(hra1)
-    t1=float(temp1)
+    hei1=  float(request.form['hei1'])
+    wei1= float( request.form['wei1'])
+    dur1=   float(request.form['dur1'])
+    hra1=  float(request.form['hra1'])
+    temp1= float(request.form['temp1'])
     
-    predi = model3.predict(np.array([[gen1,age1,h1,w1,d1,h1,t1]]))
+    predi = model3.predict(np.array([[gen1,age1,hei1,wei1,dur1,hra1,temp1]]))
     return render_template('calories.html',result = predi[0])
 
+
+@app.route('/cancer',methods=['GET','POST'])
+def can():
+    return render_template("cancer.html")
+
+@app.route('/result', methods=['POST'])
+def canresult():
+    a = float(request.form['Radius_mean'])
+    b = float(request.form['Texture_mean'])
+    c = float(request.form['Perimeter_mean'])
+    d = float(request.form['Area_mean'])
+    e =float(request.form['Smoothness_mean'])
+    f = float(request.form['Compactness_mean'])
+    g = float(request.form['Concavity_mean'])
+    h = float(request.form['concave points_mean'])
+    i =float(request.form['symmetry_mean'])
+    j = float(request.form['fractal_dimension_mean'])
+    k = float(request.form['radius_se'])
+    l = float(request.form['texture_se'])
+    m = float(request.form['perimeter_se'])
+    n = float(request.form['area_se'])
+    o = float(request.form['smoothness_se'])
+    p = float(request.form['compactness_se'])
+    q =float(request.form['concavity_se'])
+    r = float(request.form['concave points_se'])
+    s = float(request.form['symmetry_se'])
+    t = float(request.form['fractal_dimension_se'])
+    u = float(request.form['radius_worst'])
+    v = float(request.form['texture_worst'])
+    w = float(request.form['perimeter_worst'])
+    x = float(request.form['area_worst'])
+    y = float(request.form['smoothness_worst'])
+    z = float(request.form['compactness_worst'])
+    A =float(request.form['concavity_worst'])
+    B =float(request.form['concave points_worst'])
+    C =float(request.form['symmetry_worst'])
+    D =float(request.form['fractal_dimension_worst'])
+    pred = model4.predict([[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D]])
+    if pred[0] == 1:
+        predict = "You have Maligant cancer, please consult a Doctor."
+    elif predi[0] == 0:
+        predict = "You don't have cancer and you are Benign"
+    output=predict
+    
+    return render_template("cancer.html", result = output)
 
 
 
